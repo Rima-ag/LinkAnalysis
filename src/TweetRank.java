ddb96098d68fd08e11d40719e8c42f9c2f79786f
Rima-ag <rimaalghossein@gmail.com>
2021-03-21 17:02:27 +0200

    Refactors code

--- src/TweetRank.java
+++ src/TweetRank.java
@@ -1,16 +1,13 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashSet;
-import java.util.stream.Stream;
 
 public class TweetRank {
     private final Integer N = 465017;
     private final Double revN = 1 / new Double(N);
-    private Double epsilon = 1E-15;
-    private Double beta = 0.8;
+    private final Double epsilon = 1E-15;
+    private final Double beta = 0.8;
     private final Double offset = (1 - beta) * revN;
 
     private boolean firstRead = true;
@@ -25,46 +22,37 @@ public class TweetRank {
 
     public TweetRank(String filename){
         this.filename = filename;
-        if(firstRead)
-            init(r0, revN);
+        initR(r0, revN);
+        initDead();
         while(!stat) {
-            init(r1, 0.0);
+            initR(r1, 0.0);
             //Open file and parse it
             compute();
-            firstRead = false;
             append();
-//            leak();
             isStat();
-            tmp = r0;
-            r0 = r1;
-            r1 = tmp;
+            swap();
+            firstRead = false;
         }
-        Double sum = 0.;
-        for(int i = 0; i < N; ++i){
-            sum += r1[i];
-        }
-        System.out.println(sum);
-        System.out.println("Least influencer user : " + Arrays.stream(r1).min(Double::compare).toString());
-        System.out.println("Most influencer user : " + Arrays.stream(r1).max(Double::compare).toString());
 
+        result();
     }
 
-    private void init(Double[] r, Double value){
-        boolean initDead = false;
-        if (deadEnds.size() == 0)
-            initDead = true;
+    private void initR(Double[] r, Double value){
         for(int i = 0; i < N; ++i) {
             r[i] = value;
             //remove items from deadends on the first pass only
             //sum up all the old rs of deadends, devide them by N and add to all r new
             //multiply all the elements of r new with (beta + (1-beta)/N)
-            if(initDead && firstRead)
-                deadEnds.add(i);
         }
     }
 
+    private void initDead(){
+        for(int i = 0; i < N; ++i)
+            deadEnds.add(i);
+    }
+
     private void compute(){
-        Integer first, second;
+        int first, second;
         Double value;
         try{
             BufferedReader buffer = new BufferedReader(new FileReader(filename));
@@ -89,19 +77,19 @@ public class TweetRank {
     }
 
     private void handleDeadEnd(Integer notEnd){
-        if(deadEnds.contains(notEnd))
-            deadEnds.remove(notEnd);
+        deadEnds.remove(notEnd);
     }
 
-
+    // r1[first] = beta * (r1[first] + SDeadEndsRevN) + offset + [(1 - leak) * revN]
     private void append(){
         Double SDeadEndsRevN = sumDeadEnds() * revN;
         Double leak = 0.0;
-        // r1[first] = beta * (r1[first] + SDeadEndsRevN) + offset
+
         for(int i = 0; i < N; ++i){
             r1[i] = beta * (r1[i] + SDeadEndsRevN) + offset;
             leak += r1[i];
         }
+
         for(int i = 0; i < N; ++i){
             r1[i] += (1 - leak) * revN;
         }
@@ -111,16 +99,6 @@ public class TweetRank {
         return deadEnds.stream().map(s -> r0[s]).mapToDouble(Double::doubleValue).sum();
     }
 
-//    private void leak(){
-//        Double sum = 0.0;
-//        for(int i = 0; i < N; ++i){
-//            sum += r1[i];
-//        }
-//        for(int i = 0; i < N; ++i){
-//            r1[i] += (1 - sum) * revN;
-//        }
-//    }
-
     private void isStat(){
         Double diff = 0.0;
         for(int i = 0; i < N; ++i)
@@ -129,8 +107,38 @@ public class TweetRank {
             stat = true;
     }
 
+    private void swap(){
+        tmp = r0;
+        r0 = r1;
+        r1 = tmp;
+    }
+
+    private void result(){
+        Double sum = 0.;
+        Double min = r1[0];
+        int imin = 0;
+        Double max = r1[0];
+        int imax = 0;
+
+        for(int i = 0; i < N; ++i){
+            sum += r1[i];
+            if(r1[i] < min){
+                min = r1[i];
+                imin = i;
+            }
+            if(r1[i] > max){
+                max = r1[i];
+                imax = i;
+            }
+        }
+
+        System.out.println(sum);
+        System.out.println("Most influencer user : " + imax + 1 + " with a stat proba of " + max);
+        System.out.println("Least influencer user : " + imin + 1 + " with a stat proba of " + min);
+    }
+
     public static void main(String[] args){
-        TweetRank analyze = new TweetRank("/Users/rima/Desktop/LinkAnalysis/src/out2");
+        new TweetRank("out2");
     }
 
 }
